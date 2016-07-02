d41e37554d0bda15fc5cfb2bf71675985b5e63da
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-02 11:51:59 -0600

    tweak columns for user auction tables and modify test -add auction end date and payment columns to tables -reflect the change in the test -add commented out test for user can shop similar items from lost auction table; -this is an additional user story that is not top priority to implement right now

--- spec/integration/auction_dashboard_spec.rb
+++ spec/integration/auction_dashboard_spec.rb
@@ -13,8 +13,12 @@ RSpec.feature "display auction dashboard" do
     within("#open_auction") do
       expect(page).to have_content("Open Auctions")
       expect(page).to have_link(item.name)
+      expect(page).to have_content("Current High Bid")
       expect(page).to have_content(item.high_bid)
+      expect(page).to have_content("Current High Bidder")
       expect(page).to have_content(item.high_bidder.username)
+      expect(page).to have_content("Auction End Date")
+      expect(page).to have_content(item.end_time)
     end
   end
 
@@ -27,13 +31,13 @@ RSpec.feature "display auction dashboard" do
     visit dashboard_path
 
     expect(page).to have_content("No auction information yet!")
-    
+
     expect(page).to_not have_content("Open Auctions")
     expect(page).to_not have_content("Auctions Won")
     expect(page).to_not have_content("Closed Auctions")
   end
 
-  scenario "user wins an auction and sees item in the lost auctions table" do
+  scenario "user wins an auction and sees item in the won auctions table" do
     user = create(:user_with_bids)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -47,7 +51,11 @@ RSpec.feature "display auction dashboard" do
     within("#won_auction") do
       expect(page).to have_content("Auctions Won")
       expect(page).to have_link(item_won.name)
+      expect(page).to have_content("Price Paid")
       expect(page).to have_content(item_won.high_bid)
+      expect(page).to have_content("Payment Date")
+      expect(page).to have_content(item_won.end_time)
+
       expect(page).not_to have_content(item2.name)
       expect(page).not_to have_content(item3.name)
     end
@@ -71,10 +79,31 @@ RSpec.feature "display auction dashboard" do
     within("#lost_auction") do
       expect(page).to have_content("Auctions Lost")
       expect(page).to have_content(item_lost.name)
+      expect(page).to have_content("Date Auction Closed")
+      expect(page).to have_content(item_lost.end_time)
       expect(page).not_to have_content(item2.name)
       expect(page).not_to have_content(item3.name)
     end
+
+    # scenario "user can shop similar items from thier auctions lost table" do
+    #   user = create(:user_with_bids)
+    #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    #
+    #   item1, item2, item3 = user.items
+    #
+    #   user2 = create(:user)
+    #   create(:bid, price: 100, user: user2, item: item1)
+    #
+    #   item_lost = item1.tap { |item| item.update(status: "closed") }
+    #
+    #   expect(item_lost.status).to eq("closed")
+    #
+    #   visit dashboard_path
+    #
+    #   within('#lost_auction') do
+    #     expect(page).to have_content("Shop Similar Items")
+    #
+    #   end
+    # end
   end
 end
-
-
