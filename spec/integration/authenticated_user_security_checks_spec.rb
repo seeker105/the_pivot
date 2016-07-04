d59968674a200471ea6744c4d9707ec4c8659d46
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-04 13:26:28 -0600

    finish making tests pass for admins logging in

--- spec/integration/authenticated_user_security_checks_spec.rb
+++ spec/integration/authenticated_user_security_checks_spec.rb
@@ -18,7 +18,7 @@ RSpec.feature "Authenticated User has correct privledges" do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit admin_dashboard_path
+    visit business_admin_dashboard_path
 
     expect(page).to have_content("The page you were looking for doesn't exist.")
   end
