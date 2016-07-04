49a9dc412b2aa8a7da0de5a29322aefcb3a944ec
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-04 16:40:09 -0600

    add functionality so that business dashboard shows open auction info in a table -add view helper to correctly display high bidder information -add open auctions partial to busiess/dashboard/ folder

--- spec/integration/business_dashboard_shows_item_info_spec.rb
+++ spec/integration/business_dashboard_shows_item_info_spec.rb
@@ -11,4 +11,30 @@ RSpec.feature "business dashboard shows item info" do
     end
   end
 
+  scenario "for a business with open auctions" do
+    business = create(:business)
+    items = business.items << create_list(:item, 2)
+    extra_item = create(:item)
+
+    visit business_dashboard_path(business.slug)
+    save_and_open_page
+
+    within('#open_auction') do
+      expect(page).to have_content(items[0].name)
+      expect(page).to have_content(items[0].high_bid)
+      expect(page).to have_content(items[0].high_bidder)
+      expect(page).to have_content(items[0].end_time)
+
+      expect(page).to have_content(items[1].name)
+      expect(page).to have_content(items[1].high_bid)
+      expect(page).to have_content(items[1].high_bidder)
+      expect(page).to have_content(items[1].end_time)
+
+      expect(page).not_to have_content(extra_item.name)
+    end
+  end
+
+  scenario "for a business with open and closed auctions" do
+
+  end 
 end
