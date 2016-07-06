2d92aa5f64dbdb4af29127908e7d2c83b7ac0d88
Christopher Soden <karavol27@gmail.com>
2016-07-05 18:19:09 -0600

    Remove unneeded comments

--- spec/integration/only_business_admins_can_see_business_dashboards_spec.rb
+++ spec/integration/only_business_admins_can_see_business_dashboards_spec.rb
@@ -2,16 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "only business admins can see business dashboards" do
   scenario "logged in business admin visits the dashboard" do
-    # happy path:
-    # as a logged in business admin
     admin_user = create(:user)
     business = create(:business, active: true)
     admin_user.businesses << business
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
 
-    # I visit the business dashboard path for my business
-    # I see the business header on the page
     visit business_dashboard_path(slug: business.slug)
     within('body') do
       expect(page).to have_content("#{business.name} Dashboard")
@@ -19,10 +15,6 @@ RSpec.feature "only business admins can see business dashboards" do
   end
 
   scenario "logged in non-admin user visits business dashboard path" do
-    # sad path 1:
-    # a logged in user
-    # I visit the dashboard path for a business
-    # I see a 404 error
     user = create(:user)
     business = create(:business)
 
@@ -35,8 +27,6 @@ RSpec.feature "only business admins can see business dashboards" do
   end
 
   scenario "logged in business admin visits the dashboard path for a business he is NOT admin of" do
-    # sad path 2:
-    # as a logged in business admin,
     admin_user = create(:user)
     business = create(:business, active: true)
     admin_user.businesses << business
@@ -45,8 +35,6 @@ RSpec.feature "only business admins can see business dashboards" do
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
 
-    # I visit the dashboard path for a business that I am NOT the admin of
-    # I see a 404 error
     visit business_dashboard_path(slug: other_business.slug)
     within('title') do
       expect(page).to have_content("The page you were looking for doesn't exist (404)")
