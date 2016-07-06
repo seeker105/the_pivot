e4218910105ca8f15d6d2c31f1d51a10377a3a2f
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-05 21:10:03 -0600

    add feature test draft for displaying predicted selling price

---
+++ spec/integration/admin_sees_predicted_final_auction_price_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "admin sees predicted final auction price" do
+  scenario "on the item show page" do
+    admin = create(:user_with_businesses)
+    item = create(:item_with_bids)
+    # predicor = PredictorService.new(?)
+
+    allow_any_instance_of(ApplicationController).to recieve(:current_user).and_return(admin)
+
+    visit item_path(item)
+
+    within("some html") do
+      expect(page).to have_content("Predicted Selling Price: #{predictor.selling_price}")
+    end
+
+  end
+end
+
+
+#@predictor = PORO that returns the y value
+# @predictor.selling_price
