8b5e7e3bfa19b7b0a9dbc073c96d070f07ceb064
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-07 18:53:09 -0600

    display price prediction on the item show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
       "None"
     end
   end
+
+  def predict_price(item)
+    if item.bids.count >= 3
+      predictor = PredictorService.new(item)
+      "$#{predictor.predict_price.round(2)}"
+    else
+      "Not enough bids yet to make prediction"
+    end
+  end
 end
